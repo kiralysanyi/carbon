95cf643084abccff0cc204c49e44a288dc3c1585
Király Sándor <kiralysandor1986@gmail.com>
2021-09-03 19:57:23 +0200

    Base UI skeleton

---
+++ index.js
@@ -0,0 +1,41 @@
+const {app, BrowserWindow, ipcMain} = require("electron");
+
+//checking for command line parameters
+var args = process.argv;
+
+//check for parameter
+function checkParameter(name) {
+    for (var x in args) {
+        if (args[x] == name) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function initMainWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: "Carbon",
+        frame: false,
+        webPreferences: {
+            preload: __dirname + "/preload.js",
+            webviewTag: true,
+            contextIsolation: false,
+            enableRemoteModule: true
+        }
+    });
+
+    win.loadFile("app/index.html");
+    win.removeMenu();
+
+    if (checkParameter("--debug")) {
+        win.webContents.openDevTools();
+    }
+}
+
+app.whenReady().then(initMainWindow);
+
+app.on("window-all-closed", app.exit);
---
+++ preload.js
@@ -0,0 +1,19 @@
+const remote = require("electron").remote;
+const win = remote.getCurrentWindow();
+
+closewin = () => {
+    win.close();
+}
+
+minimize = () => {
+    win.minimize();
+}
+
+maximize = () => {
+    if (win.isMaximized()) {
+        win.unmaximize();
+    }
+    else {
+        win.maximize();
+    }
+}
