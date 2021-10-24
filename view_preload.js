0b63095c1fb6eefcb9c9d9c833468aa9a84d1136
Király Sándor <kiralysandor1986@gmail.com>
2021-10-24 21:54:00 +0200

    Created the carbonAPI

--- view_preload.js
+++ view_preload.js
@@ -1,6 +1,18 @@
 const {ipcRenderer} = require("electron")
-console.log("Gutten tag! Preload loaded");
+const settings = require("./settings");
 
 window.alert = (text) => {
     ipcRenderer.sendSync("alert", text);
 }
+
+carbonAPI = {};
+
+carbonAPI.getSearchEngine = () => {
+    return settings.readKeyFromFile("general.conf.json", "searchEngine");
+}
+
+carbonAPI.getVersion = () => {
+    return ipcRenderer.sendSync("getVersion");
+}
+
+console.log("Gutten tag! Preload loaded");
