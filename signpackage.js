17f1e52b3de6ebfee2f4d32f69b6920366776059
Király Sándor <kiralysandor1986@gmail.com>
2022-07-24 11:09:41 +0200

    - Changed signpackage.js to work on windows

--- signpackage.js
+++ signpackage.js
@@ -5,7 +5,7 @@ const { exec, execSync } = require("child_process");
 exports.default = async () => {
     if (fs.existsSync("./dist/win-unpacked")) {
         console.log("Signing windows package");
-        execSync("python3 -m castlabs_evs.vmp sign-pkg --persistent ./dist/win-unpacked");
+        execSync("python -m castlabs_evs.vmp sign-pkg --persistent ./dist/win-unpacked");
         console.log("Package signed.")
     } else {
         console.log("Windows package signing skipped");
