c295d5f35dd8b7899aa35cbd914c4f93c9da08b8
Király Sándor <kiralysandor1986@gmail.com>
2022-10-22 14:37:28 +0200

    - Fixed windows signing on mac

--- signpackage.js
+++ signpackage.js
@@ -5,7 +5,11 @@ const { exec, execSync } = require("child_process");
 exports.default = async () => {
     if (fs.existsSync("./dist/win-unpacked")) {
         console.log("Signing windows package");
-        execSync("python -m castlabs_evs.vmp sign-pkg --persistent ./dist/win-unpacked");
+        try {
+            execSync("python -m castlabs_evs.vmp sign-pkg --persistent ./dist/win-unpacked");
+        } catch (error) {
+            execSync("python3 -m castlabs_evs.vmp sign-pkg --persistent ./dist/win-unpacked");
+        }
         console.log("Package signed.")
     } else {
         console.log("Windows package signing skipped");
